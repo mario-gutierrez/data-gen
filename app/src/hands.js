1482324535fda1c9af80c130855a804a05eedfe7
Mario Gutierrez <mgutierrez1@logitech.com>
2025-06-18 15:53:01 +0200

    remove commented code in resize listener

--- app/src/hands.js
+++ app/src/hands.js
@@ -307,7 +307,4 @@ animate();
 
 // Handle window resize
 window.addEventListener('resize', () => {
-    // camera.aspect = window.innerWidth / window.innerHeight;
-    // camera.updateProjectionMatrix();
-    // renderer.setSize(window.innerWidth, window.innerHeight);
 });
